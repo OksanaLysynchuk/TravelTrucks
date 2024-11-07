ac5d988addff7a5738392d4396d29ed378aa2afc
OksanaLysynchuk <138123244+OksanaLysynchuk@users.noreply.github.com>
2024-11-07 20:48:38 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,13 @@
 export const Header = () => {
   return (
     <div>
-      <svg width="24" height="24">
-        <use href="/src/assets/TravelTrucks.svg" />
-      </svg>
-      <a href="../../pages/HomePage">Home</a>
-      <a href="../../pages/CatalogPage">Catalog</a>
+      <header>
+        <svg width="24" height="24">
+          <use href="/src/assets/TravelTrucks.svg" />
+        </svg>
+        <a href="../../pages/HomePage">Home</a>
+        <a href="../../pages/CatalogPage">Catalog</a>
+      </header>
     </div>
   );
 };
